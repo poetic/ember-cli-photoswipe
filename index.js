e3f99e53f9f7bd3454c2c802cfe9c4195e7a566d
Daniel Ochoa <daniel@poeticsystems.com>
2014-12-17 09:56:39 -0600

    throw error on ember serve if addon has been added but the generator has not been run/ photoswipe is missing from project.

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 /* jshint node: true */
 'use strict';
 
+var fs = require('fs');
+
 module.exports = {
-  name: 'ember-cli-photoswipe'
+  name: 'ember-cli-photoswipe',
+
+  included: function(app) {
+    this.app = app;
+
+    var psDir = app.bowerDirectory + '/photoswipe';
+
+    if (!fs.existsSync(psDir)) {
+      var msg = 'ember-cli-photoswipe: You need to run ember g photoswipe to ';
+      msg += 'install required dependencies for this addon.';
+      var err = new Error(msg);
+      err.stack = null;
+      throw err;
+    }
+  }
 };
