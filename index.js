28f93a270083b9978c424f2cbd74fbd007f5e4f6
Daniel Ochoa <daniel@poeticsystems.com>
2014-12-20 21:55:36 -0600

    Add custom build of photoswipe (without history module for now) to the generator - it interferes with ember routing.

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
 /* jshint node: true */
 'use strict';
 
-var fs = require('fs');
+var fs   = require('fs');
 var path = require('path');
 
 module.exports = {
   name: 'ember-cli-photoswipe',
 
   included: function(app) {
-    this.app = app;
-
+    this.app  = app;
     var psDir = app.bowerDirectory + '/photoswipe';
 
-    if (!fs.existsSync(psDir)) {
-      var msg = 'ember-cli-photoswipe: You need to run ember g photoswipe to ';
+    if (!fs.existsSync(psDir + '/dist/photoswipe-ember.js')) {
+      var msg = '[ember-cli-photoswipe]: You need to run ember g photoswipe to ';
       msg += 'install required dependencies for this addon.';
-      var err = new Error(msg);
-      err.stack = null;
-      throw err;
+      throw new Error(msg);
     }
 
-    app.import(psDir + '/dist/photoswipe.js');
-    app.import(psDir + '/dist/photoswipe-ui-default.min.js');
     app.import(psDir + '/dist/photoswipe.css');
     app.import(psDir + '/dist/default-skin/default-skin.css');
-    //app.import(psDir + '/dist/default-skin/default-skin.svg');
-
+    app.import(psDir + '/dist/photoswipe-ember.js');
+    app.import(psDir + '/dist/photoswipe-ui-default.min.js');
+    app.import(psDir + '/dist/default-skin/default-skin.svg');
   },
-  // modules 'History' <-- we dont want
-  //'Controller', DesktopZoom,
 
-  treeForPublic: function(tree) {
+  treeForPublic: function() {
     var svgPath = path.join(this.app.bowerDirectory, 'photoswipe', 'dist', 'default-skin');
-
     var publicTree = this.pickFiles(this.treeGenerator(svgPath), {
       srcDir: '/',
       destDir: '/assets'
     });
-
     return publicTree;
   }
 };
