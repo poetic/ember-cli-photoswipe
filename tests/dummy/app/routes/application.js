5cc3def755dc7c5e69c46bed5f144cc7d4664602
davidgoli <davigoli@gmail.com>
2016-01-25 18:28:04 -0800

    test changing items

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,34 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-  model: function() {
-    return [
-      {
-        src: 'http://placekitten.com/g/600/450',
-        w: 600, h: 450,
-        title: 'Image Description'
-      },
-      {
-        src: 'http://placekitten.com/630/600',
-        w: 630, h: 600,
-        title: 'kitty'
-      },
-      {
-        src: 'http://placekitten.com/g/450/450',
-        w: 450, h: 450,
-        title: 'more kitty'
-      },
-      {
-        src: 'http://placekitten.com/g/400/600',
-        w: 400, h: 600,
-        title: 'more more kitty'
-      },
-      {
-        src: 'http://placekitten.com/g/500/400',
-        w: 500, h: 400,
-        title: 'yup... kitty'
-      }
-    ];
-  }
-});
-
+export default Ember.Route.extend();
